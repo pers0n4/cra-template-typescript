4047914d4705ae5ed6e24478a2863d52f482dd73
Dong-Young Kim <31337.persona@gmail.com>
2020-12-23 23:41:06 +0900

    test: remove test

--- template/src/components/App/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("render hello world text", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/hello, world!/i);
-  expect(linkElement).toBeInTheDocument();
-});
